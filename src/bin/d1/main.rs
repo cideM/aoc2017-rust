4bbfda7442eb3cbc8eeec23b38602a3c44257d61
Florian Beeres <yuuki@protonmail.com>
2024-01-12 15:53:16 +0100

    Rename

--- src/bin/d1/main.rs
+++ src/bin/d1/main.rs
@@ -11,9 +11,9 @@ fn main() {
         .map(|(x, y)| if x == y { *x } else { 0 })
         .sum();
 
-    let mut five_ahead = nums.clone();
-    five_ahead.rotate_right(nums.len() / 2);
-    let p2: u32 = iter::zip(five_ahead.iter().cycle(), nums.iter())
+    let mut halfway_around = nums.clone();
+    halfway_around.rotate_right(nums.len() / 2);
+    let p2: u32 = iter::zip(halfway_around.iter().cycle(), nums.iter())
         .map(|(x, y)| if x == y { *x } else { 0 })
         .sum();
     println!("{} {}", p1, p2);
