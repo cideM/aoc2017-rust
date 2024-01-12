322f369e3727790d93cc14b0ccc760c70cff5346
Florian Beeres <yuuki@protonmail.com>
2024-01-12 15:51:59 +0100

    Cleanup D1

--- src/bin/d1/main.rs
+++ src/bin/d1/main.rs
@@ -1,25 +1,20 @@
 use std::io;
 use std::iter;
 
-fn main() -> io::Result<()> {
+fn main() {
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer)?;
+    io::stdin().read_line(&mut buffer).unwrap();
 
     let nums: Vec<u32> = buffer.chars().map(|x| x.to_digit(10).unwrap()).collect();
 
-    let iter1 = nums.iter();
-    let iter2 = iter1.clone().cycle().skip(1);
-    let p1: u32 = iter::zip(iter2, iter1)
+    let p1: u32 = iter::zip(nums.iter().cycle().skip(1), nums.iter())
         .map(|(x, y)| if x == y { *x } else { 0 })
         .sum();
-    println!("{}", p1);
 
-    let mut nums2 = nums.clone();
-    nums2.rotate_right(nums.len() / 2);
-    let iter4 = nums2.iter().cycle();
-    let p2: u32 = iter::zip(iter4, nums.iter())
+    let mut five_ahead = nums.clone();
+    five_ahead.rotate_right(nums.len() / 2);
+    let p2: u32 = iter::zip(five_ahead.iter().cycle(), nums.iter())
         .map(|(x, y)| if x == y { *x } else { 0 })
         .sum();
-    println!("{}", p2);
-    Ok(())
+    println!("{} {}", p1, p2);
 }
